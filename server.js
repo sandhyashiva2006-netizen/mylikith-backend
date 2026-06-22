e431f5a1693e21a10f32c7341dd02a9c3d04379d
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-06-23 00:05:10 +0530

    update server

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ require("./routes/auth");
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -91,4 +93,3 @@ app.listen(PORT, () => {
   );
 });
 
-app.use("/api/auth", authRoutes);
