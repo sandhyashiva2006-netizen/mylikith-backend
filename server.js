573eedc598d03810316d56159a0001605651f24b
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-07-04 18:56:10 +0530

    update server

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 require("dotenv").config();
 
-cloudinary.config({
-
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-
-    api_key:process.env.CLOUDINARY_API_KEY,
-
-    api_secret:process.env.CLOUDINARY_API_SECRET
-
-});
 
 const express = require("express");
 const cors = require("cors");
@@ -48,6 +39,14 @@ require("multer");
 
 const { v2: cloudinary } = require("cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+
+});
 
 const path =
 require("path");
